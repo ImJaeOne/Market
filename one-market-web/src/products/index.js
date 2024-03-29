4c5a22c9357c0acd8f191df9e80bf0989d6c32d4
ImJaeOne <dlawi0108@naver.com>
2024-03-29 19:56:59 +0900

    One Market Front
    products page

--- one-market-web/src/products/index.js
+++ one-market-web/src/products/index.js
@@ -3,8 +3,120 @@ import './index.css';
 function ProductsPageComponent() {
     return (
         <div>
+            <h1 id="product-headline">중고거래</h1>
             <section id="products">
-                <h1 id="product-headline">중고거래</h1>
+                <div className="product-card">
+                    <div>
+                        <img className="product-img" src="images/notebook1.jpg" alt="product-img" />
+                    </div>
+                    <div className="product-contents">
+                        <span className="product-name">맥북 16인치</span>
+                        <span className="product-price">300000원</span>
+                        <div className="product-footer">
+                            <div className="product-seller">
+                                <img className="product-avatar" src="images/avatar.png" alt="profile-img" />
+                                <span>임재원</span>
+                            </div>
+                            <span className="product-date">2024-03-26</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="product-card">
+                    <div>
+                        <img className="product-img" src="images/notebook1.jpg" alt="product-img" />
+                    </div>
+                    <div className="product-contents">
+                        <span className="product-name">맥북 16인치</span>
+                        <span className="product-price">300000원</span>
+                        <div className="product-footer">
+                            <div className="product-seller">
+                                <img className="product-avatar" src="images/avatar.png" alt="profile-img" />
+                                <span>임재원</span>
+                            </div>
+                            <span className="product-date">2024-03-26</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="product-card">
+                    <div>
+                        <img className="product-img" src="images/notebook1.jpg" alt="product-img" />
+                    </div>
+                    <div className="product-contents">
+                        <span className="product-name">맥북 16인치</span>
+                        <span className="product-price">300000원</span>
+                        <div className="product-footer">
+                            <div className="product-seller">
+                                <img className="product-avatar" src="images/avatar.png" alt="profile-img" />
+                                <span>임재원</span>
+                            </div>
+                            <span className="product-date">2024-03-26</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="product-card">
+                    <div>
+                        <img className="product-img" src="images/notebook1.jpg" alt="product-img" />
+                    </div>
+                    <div className="product-contents">
+                        <span className="product-name">맥북 16인치</span>
+                        <span className="product-price">300000원</span>
+                        <div className="product-footer">
+                            <div className="product-seller">
+                                <img className="product-avatar" src="images/avatar.png" alt="profile-img" />
+                                <span>임재원</span>
+                            </div>
+                            <span className="product-date">2024-03-26</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="product-card">
+                    <div>
+                        <img className="product-img" src="images/notebook1.jpg" alt="product-img" />
+                    </div>
+                    <div className="product-contents">
+                        <span className="product-name">맥북 16인치</span>
+                        <span className="product-price">300000원</span>
+                        <div className="product-footer">
+                            <div className="product-seller">
+                                <img className="product-avatar" src="images/avatar.png" alt="profile-img" />
+                                <span>임재원</span>
+                            </div>
+                            <span className="product-date">2024-03-26</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="product-card">
+                    <div>
+                        <img className="product-img" src="images/notebook1.jpg" alt="product-img" />
+                    </div>
+                    <div className="product-contents">
+                        <span className="product-name">맥북 16인치</span>
+                        <span className="product-price">300000원</span>
+                        <div className="product-footer">
+                            <div className="product-seller">
+                                <img className="product-avatar" src="images/avatar.png" alt="profile-img" />
+                                <span>임재원</span>
+                            </div>
+                            <span className="product-date">2024-03-26</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="product-card">
+                    <div>
+                        <img className="product-img" src="images/notebook1.jpg" alt="product-img" />
+                    </div>
+                    <div className="product-contents">
+                        <span className="product-name">맥북 16인치</span>
+                        <span className="product-price">300000원</span>
+                        <div className="product-footer">
+                            <div className="product-seller">
+                                <img className="product-avatar" src="images/avatar.png" alt="profile-img" />
+                                <span>임재원</span>
+                            </div>
+                            <span className="product-date">2024-03-26</span>
+                        </div>
+                    </div>
+                </div>
                 <div className="product-card">
                     <div>
                         <img className="product-img" src="images/notebook1.jpg" alt="product-img" />
